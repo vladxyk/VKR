91b8b8aed404a60633a7ed708315a95b96350ad4
vladxyk <47657839+vladxyk@users.noreply.github.com>
2022-03-09 20:25:36 +0700

    fix header

--- header.hpp
+++ header.hpp
@@ -6,7 +6,7 @@ void init_matrix(double *matrix, int n);
 
 void step1(double *matrix, int n);
 void step2(double *matrix, int n);
-void step3(double *matrix, int *lines, int *zerosRow, int *zerosCol, int n);
+void step3(double *matrix, int *lines, int *arrAssigned, int n);
 void step4(double *matrix, int *lines, int n);
 
 void print_matrix(double *matrix, int n);
