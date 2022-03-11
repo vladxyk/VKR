05604c4e3f39741584bf1b2b6f2b2ecd4acace20
vladxyk <47657839+vladxyk@users.noreply.github.com>
2022-03-11 15:42:03 +0700

    edit header

--- header.hpp
+++ header.hpp
@@ -4,10 +4,18 @@
 
 void init_matrix(double *matrix, int n);
 
-void step1(double *matrix, int n);
-void step2(double *matrix, int n);
+int step1(double *matrix, int n, int *arrRowIndex, double *arrValue, int *arrCol, int NZ);
+int step2(double *matrix, int n, int *arrRowIndex, double *arrValue, int *arrCol, int NZ);
 void step3(double *matrix, int *lines, int *arrAssigned, int n);
 void step4(double *matrix, int *lines, int n);
+
+void CSR_arr_value(double *arrValue, double *mat, int n);
+void CSR_arr_col(int *arrCol, double *mat, int n, int NZ);
+void CSR_arr_row_index(int *arrRowIndex, double *mat, int n);
+
+void print_arr_value(double *arrValue, int NZ);
+void print_arr_col(int *arrCol, int NZ);
+void print_arr_row_index(int *arrRowIndex, int n);
 
 void print_matrix(double *matrix, int n);
 
